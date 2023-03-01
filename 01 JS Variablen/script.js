399733b9dae407adbd1e962ee174c5b14e087181
Sarah <sarah.wolf@htldornbirn.at>
2023-03-01 13:02:44 +0100

    exercise completed (?)

--- 01 JS Variablen/script.js
+++ 01 JS Variablen/script.js
@@ -9,3 +9,23 @@ let c = 10;
 let result = a + b + c;
 console.log (result);
 
+result = (a + b)/c;
+console.log (result);
+
+result = c - a + b;
+console.log(result);
+
+result = a + b + c * 3;
+console.log(result);
+
+result = (a + b + c)/4;
+console.log(result);
+
+a = 3;
+console.log(a);
+
+b= a + 3
+console.log(b);
+
+c = c-3
+console.log(c);
