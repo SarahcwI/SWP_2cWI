76477c18f38c846d0fd2f23e9552e9575e2119c0
Sarah <sarah.wolf@htldornbirn.at>
2023-03-22 09:34:12 +0100

    Exercise5_started_finished_Exc_1

---
+++ 05_If-Bedingung/script.js
@@ -0,0 +1,43 @@
+//Video
+
+let number = 70;
+
+if (number < 25) {
+    console.log("Die Zahl ist größer als 20") 
+}
+
+else if (number < 50){
+    console.log("kleiner als 50 " + number)
+}
+
+else {
+    console.log("else");
+}
+
+
+//Aufgabe 1
+
+// Wenn die Zahl kleiner ist als 20  gib aus "Mini"
+let number1 = 15;
+if (number1 < 20) {
+    console.log("mini");
+}
+
+// Wenn die Zahl zw. 20 und 50 ist gib aus "Medium"
+let number2 = 35;
+if (number2 > 20 && number2 < 50) {
+    console.log("Medium");
+}
+
+// Wenn die Zahl größer als 50 ist gib aus "Large"
+let number3 = 60;
+if (number3 > 50) {
+    console.log("Large");
+}
+
+
+//Aufgabe 2
+
+// Wenn die Zahl kleiner gleich 7 ist gib aus "Mini"
+// Wenn die Zahl zwischen 7 und 90 ist gib  aus "Medium"
+// Wenn die Zahl größer als 90 ist gib aus "Large"
