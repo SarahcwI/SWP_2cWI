5931ad96ac2dd92681351e197896cffb479c3edb
Sarah <sarah.wolf@htldornbirn.at>
2023-03-22 09:04:36 +0100

    Exercise_04_Finished

---
+++ 04_Schleifen/script.js
@@ -0,0 +1,62 @@
+//Videos
+console.log("Test");
+
+for (let count = 10; count > 0; count--) {
+    console.log(count + " Andreas");
+};
+
+console.log("Fertig");
+
+//Aufgaben
+//Zähle von 7 bis 45 
+console.log("Aufgabe 1");
+
+for (let count1 = 7; count1 < 46; count1++) {
+    console.log(count1);
+};
+
+
+// Zähle von 9 bis 99
+console.log("Aufgabe 2");
+
+for (let count2 = 9; count2 < 100; count2++) {
+    console.log(count2);
+};
+
+
+// Zähle rückwärts von 10 bis 0
+console.log("Aufgabe 3");
+
+for (let count3 = 10; count3 > -1; count3--) {
+    console.log(count3);
+};
+
+
+// Zähle rückwärts von 1234 bis 1207
+console.log("Aufgabe 4");
+
+for(let count4 = 1234; count4 > 1206; count4--) {
+    console.log(count4);
+};
+
+
+// Gib Deinen Namen genau 8 mal aus
+console.log("Aufgabe 5");
+
+for(let nameCount = 0; nameCount < 8; nameCount++){
+    console.log("Sarah");
+};
+
+
+// Zähle von 0 auf 10, dann gib Deinen Namen aus, dann zähle von 10 auf 0
+console.log("Aufgabe 6");
+
+for(let count5 = 0; count5 < 11; count5++){
+    console.log(count5)
+}; 
+
+console.log("Sarah");
+
+for(let count6 = 10; count6 > -1; count6--){
+    console.log(count6);
+};
