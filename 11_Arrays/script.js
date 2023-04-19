56c2319d78185e8b7125b212d5c5cb182eef1512
Sarah <sarah.wolf@htldornbirn.at>
2023-04-19 10:14:21 +0200

    Exercise_11_started_finished_Exercise_1

---
+++ 11_Arrays/script.js
@@ -0,0 +1,48 @@
+/*let a = 4;
+let b = 9;
+let c = 8;
+
+let arr = [4,9,8];
+//console.log(arr[1]);
+
+arr.push(10);
+arr.push(3);
+
+
+for (let index = 0; index < arr.length; index++) {
+    console.log(arr[index]);
+}*/
+
+//Afugabe 1
+
+let array = [4,1,2,3];
+
+array.push(17);
+array.push(199);
+
+//a
+/*
+for (let index = 0; index < array.length; index++) {
+    console.log(array[index]);
+    
+}*/
+
+
+//b 
+let summe = 0
+for(let index2 = 0; index2 < array.length; index2++) {
+      
+    summe += array[index2];
+    
+}
+console.log("Summe: " + summe);
+
+
+//c
+let summe2 = 0
+for(let index3 = 0; index3 < array.length; index3++) {
+      
+    summe2 += array[index3];
+    
+}
+console.log("Mittelwert: " + summe2/6);
