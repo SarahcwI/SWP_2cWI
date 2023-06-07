60a8856af835b9c81093132c1fd9d4662917a364
Sarah <sarah.wolf@htldornbirn.at>
2023-06-07 08:22:06 +0200

    Updated_Commits

--- 13_Bankomat_Beispiel/script.js
+++ 13_Bankomat_Beispiel/script.js
@@ -23,7 +23,6 @@ while (isGameRunning) {
   printMenu();
   console.log("-----------");
   let userInput = await readLineAsync();
- 
 
   if (userInput == 1) {
     console.log("Geben Sie den Betrag ein den Sie einzahlen möchten:");
@@ -32,8 +31,6 @@ while (isGameRunning) {
     console.log("Sie haben " + inputOfUserEinzahlung + " Euro eingezahlt");
   }
 
-
-
   else if (userInput == 2) {
     console.log("Geben Sie den Betrag ein den Sie abheben möchten:");
     let inputOfUserAbhebung = await readLineAsync();
@@ -41,21 +38,17 @@ while (isGameRunning) {
     console.log("Sie haben " + inputOfUserAbhebung + " Euro abgehoben");
   }
   
-  
   else if (userInput == 3) {
     console.log("Ihr Kontostand beträgt: " + balance);
   }
-  
   
   else if (userInput == 4) {
     console.log("Maschine stoppt!");
     isGameRunning=false;
     readline.close();
-  }
-  
-
-  
+  } 
 }
+
 
 console.log("Goodbye!");
 
