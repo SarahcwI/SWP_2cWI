7b8365fb3660e8dbace2df078a3dc106277efdc3
Sarah <sarah.wolf@htldornbirn.at>
2023-06-07 08:58:32 +0200

    Exercise_14_function_printField_working

--- 14_Tic_Tac_Toe/script.js
+++ 14_Tic_Tac_Toe/script.js
@@ -1,19 +1,26 @@
 let field = [
     [0,0,0],
     [0,0,0],
-    [0,0,0],
+    [0,0,0]
 ];
 
 //console.log(field[2][2]);
 
 
-for (let row=0; row<field.length; row++){
-    let actualRow = field[row];
-    let output = "";
-    for(let col = 0; col<actualRow.length; col++){
-        //console.log(actualRow[col]);
-        output += actualRow[col] + " ";
-    }
-    console.log(output);
-}
+printField();
 
+field[0][1] = 1;
+
+printField();
+
+function printField(){
+    for (let row=0; row < field.length; row++){
+        let actualRow = field[row];
+        let output = "";
+        for(let col = 0; col<actualRow.length; col++){
+            //console.log(actualRow[col]);
+            output += actualRow[col] + " ";
+        }
+        console.log(output);
+    }
+}
