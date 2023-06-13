0df6927aeaf4b412ec060ff37f8f65dd889e6363
Sarah <sarah.wolf@htldornbirn.at>
2023-06-13 21:11:45 +0200

    Ex_14_Added_new_Folder_&_Switching_Player_Working

---
+++ 14_TicTacToe/script.js
@@ -0,0 +1,88 @@
+import { log } from "console";
+import { createInterface } from "readline";
+
+const readline = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const readLineAsync = () => {
+  return new Promise((resolve) => {   
+    readline.question("", (userRes) => {
+      resolve(userRes);
+      //
+    });
+  });
+};
+
+
+
+
+
+let field = [
+    [0,0,0],
+    [0,0,0],
+    [0,0,0]
+];
+
+//console.log(field[2][2]);
+
+//let inputPlayerRow = 2;
+//let inputPlayerCol = 1;
+
+//field[inputPlayerRow][inputPlayerCol] = 0;
+
+
+
+function printField(){
+    for (let row=0; row < field.length; row++){
+        let actualRow = field[row];
+
+        let output = "";
+        for(let col = 0; col<actualRow.length; col++){
+            //console.log(actualRow[col]);
+            let sign=" ";
+            if (actualRow[col] == 1){
+                sign="X";
+            }
+            else if (actualRow[col] == 2){
+                sign ="O";
+            }
+            output += sign + "|";
+        }
+        console.log(output);
+    }
+}
+
+
+let playerPlaying = 0;
+
+for (let county = 0; county <9; county++) {
+    if (county%2==0){
+        playerPlaying = 1;
+    }
+    
+    else{playerPlaying = 2;}
+    
+    console.log(playerPlaying);
+
+    console.log("Reihe:");
+    let inputPlayerRow = await readLineAsync();
+    console.log("Spalte:"); 
+    let inputPlayerColumn = await readLineAsync();
+    
+    field[inputPlayerRow][inputPlayerColumn] = playerPlaying;
+    printField();   
+    
+
+   
+}
+
+
+
+
+
+
+
+readline.close();
+
