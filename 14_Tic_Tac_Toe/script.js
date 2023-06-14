faedaa0162e6aee7710e79a693408117cc495394
Sarah <sarah.wolf@htldornbirn.at>
2023-06-14 09:19:33 +0200

    Ex_14_Player_Winning_working_one_time

--- 14_Tic_Tac_Toe/script.js
+++ 14_Tic_Tac_Toe/script.js
@@ -7,7 +7,7 @@ const readline = createInterface({
 });
 
 const readLineAsync = () => {
-  return new Promise((resolve) => {   
+  return new Promise((resolve) => {
     readline.question("", (userRes) => {
       resolve(userRes);
       //
@@ -17,9 +17,9 @@ const readLineAsync = () => {
 
 
 let field = [
-  [0,0,0],
-  [0,0,0],
-  [0,0,0]
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0]
 ];
 
 //console.log(field[2][2]);
@@ -29,43 +29,58 @@ let field = [
 
 
 
-function printField(){
-  for (let row=0; row < field.length; row++){
-      let actualRow = field[row];
+function printField() {
+  for (let row = 0; row < field.length; row++) {
+    let actualRow = field[row];
 
-      let output = "";
-      for(let col = 0; col<actualRow.length; col++){
-          let sign=" ";
-          if (actualRow[col] == 1){
-              sign="X";
-          }
-          else if (actualRow[col] == 2){
-              sign ="O";
-          }
-          output += sign + "|";
+    let output = "";
+    for (let col = 0; col < actualRow.length; col++) {
+      let sign = " ";
+      if (actualRow[col] == 1) {
+        sign = "X";
       }
+      else if (actualRow[col] == 2) {
+        sign = "O";
+      }
+      output += sign + "|";
+    }
     console.log(output);
   }
 }
 
-
+let isGameRunning = true;
 let playerPlaying = 0;
+let counter;
 
-for (let counter = 0; counter <9; counter++) {
-  if (counter%2==0){
+while (isGameRunning) {
+  for (counter = 0; counter < 9; counter++) {
+    if (counter % 2 == 0) {
       playerPlaying = 1;
-  }
-  else{playerPlaying = 2;}
+    }
+    else { playerPlaying = 2; }
 
-  console.log(playerPlaying);
-  console.log("Reihe:");
-  let inputPlayerRow = await readLineAsync();
-  console.log("Spalte:"); 
-  let inputPlayerColumn = await readLineAsync();
-  
-  field[inputPlayerRow][inputPlayerColumn] = playerPlaying;
-  printField();   
+    console.log("It's Player " + playerPlaying + "s turn");
+    console.log("Reihe:");
+    let inputPlayerRow = await readLineAsync();
+    console.log("Spalte:");
+    let inputPlayerColumn = await readLineAsync();
+
+    field[inputPlayerRow][inputPlayerColumn] = playerPlaying;
+    printField();
+
+    if (field[0][0] == field[0][1] == field[0][2] && counter > 2) {
+      endGame();
+    }
+  }
 }
 
-readline.close();
+function inputPlayer(){
+  
+}
 
+function endGame() {
+  console.log("Player" + playerPlaying + " Won!");
+  counter =8;
+  isGameRunning = false;
+  readline.close();
+}
